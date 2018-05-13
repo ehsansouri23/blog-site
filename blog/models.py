0576b354a27f6738fdda86628d490c6229d609a9
ehsan souri <ehsansouri23@gmail.com>
2018-05-13 23:13:42 +0430

    models added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    date_created = models.DateTimeField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
